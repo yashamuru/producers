d46e83fecec927fe9069be550402291c081c9b3a
Tsvetan Nenov <yashamuru@gmail.com>
2017-08-13 16:34:24 +0200

    ProducerBundle instead of AppBundle

---
+++ src/ProducerBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ProducerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('ProducerBundle:Default:index.html.twig');
+    }
+}
---
+++ src/ProducerBundle/ProducerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ProducerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ProducerBundle extends Bundle
+{
+}
